0bc269a64bbb2dce1759b51e509fb0882da2e16b
tebogo-4-dee <mapetlatebogo@gmail.com>
2023-10-11 20:45:39 -0700

    7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
- */
-int _abs(int n)
-{
-	if (n >= 0)
-	{
-		return (n);
-	}
-	return (-n);
-#include "main.h"
-
-/**
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
  * Return: value of the last digit
@@ -34,4 +20,4 @@ int print_last_digit(int n)
 	_putchar(a + '0');
 
 	return (a);
-}}
+}
